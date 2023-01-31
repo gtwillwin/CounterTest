680c558d0dcf963d9af4b30a995f5ab1a6fba8a3
William Wynne <wwynne3@gatech.edu>
2023-01-31 10:28:14 -0500

    initial commit

---
+++ src/main/java/com/example/counter/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.awt.font.TextAttribute
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val counter = findViewById<TextView>(R.id.counter)
+
+        counter.text = """$count"""
+
+        val plusbtn = findViewById<Button>(R.id.plusbtn)
+        val minusbtn = findViewById<Button>(R.id.minusbtn)
+
+        plusbtn.setOnClickListener {
+            counter.text = """${++count}"""
+        }
+
+        minusbtn.setOnClickListener {
+            counter.text = """${--count}"""
+        }
+    }
+}
